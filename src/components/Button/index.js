96c5a8d2e58c0ad6f99f4ddf7fce895a49a38d77
Your Name <alexsigala87@gmail.com>
2020-01-20 16:07:19 -0800

    added giphy API folder. linked to button need to write handle input change and submit

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
 import "./style.css";
 import { ButtonToolbar, Button } from "react-bootstrap";
+import API from "../../utils/API";
 
 class Switch extends Component {
     stopLights = () => {
         console.log(`clicked button class ${this.props.className}`);
     };
+
     render() {
         return (
-            <div className="buttonDiv">
-                <ButtonToolbar>
-                    <Button variant="danger" onClick={this.stopLights}>
-                        Danger
-                    </Button>
-                </ButtonToolbar>
+            <div>
+                <div className="buttonDiv">
+                    <ButtonToolbar>
+                        <Button variant="danger" onClick={this.stopLights}>
+                            Danger
+                        </Button>
+                    </ButtonToolbar>
+                </div>
+
+                <div className="gifDiv"></div>
             </div>
         );
     }
